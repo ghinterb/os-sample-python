85b23e912d0051b5889c2f6cee5dae995af25fd8
greghint <greghint@DC071621.idir.BCGOV>
2018-03-07 09:43:42 -0800

    added image

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World! - My Name is Cyril Figgis"
+    return "Hello World! - My Name is Cyril Figgis<br/><img src='http://i.imgur.com/aypgP.jpg'/>"
 
 if __name__ == "__main__":
     application.run()
